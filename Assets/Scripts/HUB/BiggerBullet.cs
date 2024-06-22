d5caf72f95797c95f83854528f83b437c5d93e14
Aiden Thacker <aidenthacker86@gmail.com>
2024-06-22 14:59:49 -0500

    Added Money and the Ability to Switch Weapons at the HUB
    Currently behind so the actually is not connected yet

---
+++ Assets/Scripts/HUB/BiggerBullet.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperPupSystems.Manager;
+
+public class BiggerBullet : MonoBehaviour
+{
+    public GameObject purchaseTag;
+    public int price = 100;
+    public bool insideCollider = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        purchaseTag.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(insideCollider == true)
+        {
+            purchaseTag.SetActive(true);
+        }else
+        {
+            purchaseTag.SetActive(false);
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            insideCollider = true;
+            if(Input.GetKeyDown(KeyCode.F) && WalletManager.instance.coin >= price)
+            {
+                Debug.Log("Player Buys the Bigger Bullet Gun");
+                SwitchGuns(other.transform, "BiggerBulletGun");
+
+                WalletManager.instance.coin -= price;
+            }
+            
+        }
+    }
+
+    public void OnTriggerExit(Collider other) 
+    { 
+        if(other.gameObject.CompareTag("Player"))
+        {
+            insideCollider = false;
+        }
+    }
+
+    private void SwitchGuns(Transform playerTransform, string gunName)
+    {
+        Transform gunHolder = playerTransform.Find("PlayerCamera/GunHolder");
+        if (gunHolder != null)
+        {
+            foreach (Transform gun in gunHolder)
+            {
+                gun.gameObject.SetActive(false);
+            }
+
+            Transform purchasedGun = gunHolder.Find(gunName);
+            if (purchasedGun != null)
+            {
+                purchasedGun.gameObject.SetActive(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/HUB/FullyAuto.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SuperPupSystems.Manager;
+
+public class FullyAuto : MonoBehaviour
+{
+    public GameObject purchaseTag;
+    public int price = 100;
+    public bool insideCollider = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        purchaseTag.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(insideCollider == true)
+        {
+            purchaseTag.SetActive(true);
+        }else
+        {
+            purchaseTag.SetActive(false);
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            insideCollider = true;
+            if(Input.GetKeyDown(KeyCode.F) && WalletManager.instance.coin >= price)
+            {
+                Debug.Log("Player Buys The Fully Auto");
+                SwitchGuns(other.transform, "FullyAutoGun");
+                WalletManager.instance.coin -= price;
+            }
+            
+        }
+    }
+
+    public void OnTriggerExit(Collider other) 
+    { 
+        if(other.gameObject.CompareTag("Player"))
+        {
+            insideCollider = false;
+        }
+    }
+
+    private void SwitchGuns(Transform playerTransform, string gunName)
+    {
+        Transform gunHolder = playerTransform.Find("PlayerCamera/GunHolder");
+        if (gunHolder != null)
+        {
+            foreach (Transform gun in gunHolder)
+            {
+                gun.gameObject.SetActive(false);
+            }
+
+            Transform purchasedGun = gunHolder.Find(gunName);
+            if (purchasedGun != null)
+            {
+                purchasedGun.gameObject.SetActive(true);
+            }
+        }
+    }
+}
