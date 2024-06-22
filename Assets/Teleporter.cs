3fba9b96b724f2f255be3f34564df0d517084173
Logan Hobson <Loganhobson31@gmail.com>
2024-06-22 18:28:45 -0500

    Added health stuff to enemies. Added death fall through.

--- Assets/Teleporter.cs
+++ Assets/Teleporter.cs
@@ -7,13 +7,25 @@ public class Teleporter : MonoBehaviour
 {
     public Transform destination;
     public UnityEvent onTeleport;
+
+    public bool death = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !death)
         {
             Debug.Log("Teleported");
             other.gameObject.transform.position = destination.position;
             onTeleport.Invoke();
+        }
+
+        if(death)
+        {
+            if(destination == null)
+            {
+                destination = GameObject.Find("Destination").transform;
+                other.gameObject.transform.position = destination.position;
+                GameObject.Find("GameManager").GetComponent<GameManager>().ClearRooms();
+            }
         }
     }
 }
