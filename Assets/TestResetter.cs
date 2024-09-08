23419a63ecd854e556f2badf2a10aff4f8e46c3f
Logan Hobson <Loganhobson31@gmail.com>
2024-09-07 22:21:18 -0500

    Resetting logic.

--- Assets/TestResetter.cs
+++ Assets/TestResetter.cs
@@ -2,16 +2,36 @@ using SolarStudios;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TestResetter : MonoBehaviour
 {
     public ObjectPool pool;
-
+    public UnityEvent evnt;
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
+            foreach(GameObject obj in pool.objectPool)
+            {
+                if(obj.activeInHierarchy)
+                {
+                    fixRooms(obj);
+                }
+            }
             pool.RecycleAll();
+            evnt.Invoke();
         }
+    }
+
+    void fixRooms(GameObject obj)
+    {
+         //I know. This is just getting the list of rooms from each pool.
+        
+            foreach (Transform child in obj.transform)
+            {
+                child.gameObject.SetActive(true);
+            }
+        
     }
 }
