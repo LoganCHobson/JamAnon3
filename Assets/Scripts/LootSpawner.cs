f5562e92e6f765264af23c6ac4d4d7c5271c2b4a
Logan Hobson <Loganhobson31@gmail.com>
2024-09-29 21:45:22 -0500

    Loot Spawner. Player shoots at crooshair. Enemy balancing.

---
+++ Assets/Scripts/LootSpawner.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class LootItem
+{
+    public GameObject itemPrefab;  
+    [Range(0f, 1f)] public float spawnProbability;
+}
+
+public class LootSpawner : MonoBehaviour
+{
+    public List<LootItem> lootTable = new List<LootItem>(); 
+
+    public void SpawnLoot()
+    {
+        LootItem selectedLoot = GetRandomLoot();
+        if (selectedLoot != null)
+        {
+            
+            Instantiate(selectedLoot.itemPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            Debug.Log("No loot spawned.");
+        }
+    }
+
+    private LootItem GetRandomLoot()
+    {
+        float totalWeight = 0f;
+        
+        foreach (LootItem loot in lootTable)
+        {
+            totalWeight += loot.spawnProbability;
+        }
+
+        float randomValue = Random.Range(0f, totalWeight);
+        
+        float cumulativeWeight = 0f;
+        foreach (LootItem loot in lootTable)
+        {
+            cumulativeWeight += loot.spawnProbability;
+            if (randomValue <= cumulativeWeight)
+            {
+                return loot;
+            }
+        }
+       
+        return null;
+    }
+}
